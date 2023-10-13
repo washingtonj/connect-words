0e222553e5131d09b92af0645434234b30c6b563
Washington Junior <wwnjr@icloud.com>
2023-10-13 20:06:44 -0300

    fix the percentage calc

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
+import Link from 'next/link'
 import { useEffect } from "react";
 import { useSearchParams, useRouter } from 'next/navigation'
-import Link from 'next/link'
 import { Button } from "@/components/ui/button";
 
 export default function Result() {
@@ -12,8 +12,7 @@ export default function Result() {
   const queryData = query.get('v')
 
   const { attempts, combinations } = JSON.parse(atob(queryData || '') || '{}')
-
-  const porcentageOfHits = Math.round((combinations.length / 6) * 100)
+  const percentage = ((combinations.length / attempts) * 100).toFixed(1)
 
   useEffect(() => { if (!queryData) router.push('/') }, [queryData, router])
 
@@ -21,7 +20,7 @@ export default function Result() {
     <div className="h-screen flex flex-col items-center justify-center px-8 lg:w-4/12 lg:m-auto">
       <span className="text-4xl mb-8">🏆</span>
       <h1 className="pb-4"><b>Parabéns</b>, você encontrou todos os tópicos!</h1>
-      <p>Você teve <b>{porcentageOfHits}%</b> de aproveitamento, conseguindo ligar todas as palavras em <b>{attempts} tentativas.</b></p>
+      <p>Você teve <b>{percentage}%</b> de aproveitamento, conseguindo ligar todas as palavras em <b>{attempts} tentativas.</b></p>
       <span className="mt-4">
         <p className="capitalize"><b>Tópicos:</b> {combinations.join(', ')}</p>
       </span>
