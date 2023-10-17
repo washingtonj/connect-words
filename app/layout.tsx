8d363ca89f515cae77620c1dee69acbc5396c87f
Washington Junior <wwnjr@icloud.com>
2023-10-17 11:01:25 -0300

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Brennas Connect',
-  description: 'Encontre palavras conexas em português',
+  description: 'Encontre palavras conexas em português'
 }
 
 export default function RootLayout({
