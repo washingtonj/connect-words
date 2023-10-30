5978cb5a4570cc100e5bffa6b3a4a5eec2917b92
Washington Junior <wwnjr@icloud.com>
2023-10-29 22:52:01 -0300

    improve ranking page

--- app/ranking/page.tsx
+++ app/ranking/page.tsx
@@ -21,68 +21,65 @@ const getServerSideProps = cache(async () => {
 })
 
 export default async function Ranking() {
-  // Use prisma to get only the first 10 users with the highest score
   const data = await getServerSideProps()
-
-  // Create the date that the ranking will be updated
-  const date = new Date()
-  date.setSeconds(date.getSeconds() + REAVALIDATE_TIME)
-
+  const nextUpdate = new Date(new Date().getTime() + (REAVALIDATE_TIME * 1000))
   const hasData = data.length > 0
 
   return (
-    <div className='flex flex-col gap-8 pt-8 px-4 lg:max-w-2xl text-sm pb-10 lg:m-auto'>
-      <Logo />
-      <p>Veja quem são os 10 melhores jogadores do jogo, você também pode estar aqui!</p>
-
-      <div>
-        <p className="text-sm text-gray-500">Próxima atualização: <b><DateTime date={date} /></b></p>
+    <div className='pt-8 px-4 lg:max-w-2xl pb-10 lg:m-auto'>
+      <div className='flex items-center justify-between mb-12'>
+        <Logo />
+        <div>
+          <p className="text-sm text-gray-500">Próxima atualização: <b><DateTime date={nextUpdate} /></b></p>
+        </div>
       </div>
 
-      {
-        hasData && (
-          <>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="text-center">Nome</TableHead>
-                  <TableHead className="text-center">Tempo</TableHead>
-                  <TableHead className="text-center">Tentativas</TableHead>
-                  <TableHead className="text-center">Pontos</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {
-                  data.map((user, index) => (
-                    <TableRow key={index}>
-                      <TableCell className="text-center">{user.author || '👻'}</TableCell>
-                      <TableCell className="text-center">{user.time}</TableCell>
-                      <TableCell className="text-center">{user.attempts}</TableCell>
-                      <TableCell className="text-center">{user.points}</TableCell>
-                    </TableRow>
-                  ))
-                }
-              </TableBody>
-            </Table>
+      <div className='flex flex-col gap-8 text-sm'>
+        {
+          hasData && (
+            <>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="text-center">Nome</TableHead>
+                    <TableHead className="text-center">Tempo</TableHead>
+                    <TableHead className="text-center">Tentativas</TableHead>
+                    <TableHead className="text-center">Pontos</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {
+                    data.map((user, index) => (
+                      <TableRow key={index}>
+                        <TableCell className="text-center">{user.author || '👻'}</TableCell>
+                        <TableCell className="text-center">{user.time}</TableCell>
+                        <TableCell className="text-center">{user.attempts}</TableCell>
+                        <TableCell className="text-center">{user.points}</TableCell>
+                      </TableRow>
+                    ))
+                  }
+                </TableBody>
+              </Table>
 
 
-            <span>
-              <p><b>Tempo</b> é o tempo que o jogador levou para completar o jogo.</p>
-              <p><b>Pontos</b> é a pontuação que o jogador obteve com base na dificuldade dos tópicos.</p>
-            </span>
-          </>
-        )
-      }
+              <span>
+                <p><b>Tempo</b> é o tempo que o jogador levou para completar o jogo.</p>
+                <p><b>Pontos</b> é a pontuação que o jogador obteve com base na dificuldade dos tópicos.</p>
+              </span>
+            </>
+          )
+        }
 
 
-      {
-        !hasData && (
-          <div className="flex flex-col items-center justify-center gap-4">
-            <h1 className="text-2xl font-bold">Nenhum dado encontrado.</h1>
-            <p className="text-sm text-gray-500">Tente novamente mais tarde.</p>
-          </div>
-        )
-      }
+        {
+          !hasData && (
+            <div className="flex flex-col items-center justify-center gap-4">
+              <h1 className="text-2xl font-bold">Nenhum dado encontrado.</h1>
+              <p className="text-sm text-gray-500">Tente novamente mais tarde.</p>
+            </div>
+          )
+        }
+      </div>
     </div>
   )
 }
